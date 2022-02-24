c0eeaf5cbae64d1f4965b469c1457489b053b534
Anton Churkin <praetoriaixleg@gmail.com>
2022-02-24 14:37:08 +0300

    [+] Добавлен pipfile

--- backend/tests/models.py
+++ backend/tests/models.py
@@ -9,8 +9,8 @@ class BaseModel(models.Model):
 
 
 class VariantOfAnswer(BaseModel):
-    value_of_answer = models.TextField(
-        blank=True, verbose_name='Вариант ответа')
+    # Убрать blank
+    value_of_answer = models.TextField(verbose_name='Вариант ответа')
 
     def __str__(self):
         return self.value_of_answer
@@ -21,12 +21,14 @@ class VariantOfAnswer(BaseModel):
         verbose_name_plural = 'Варианты ответа'
 
 
+
+
 class Question(BaseModel):
-    body_question = models.TextField(blank=True, verbose_name='Тело вопроса')
+    body_question = models.TextField(verbose_name='Тело вопроса')
     category = models.ForeignKey(
-        'Category', on_delete=models.CASCADE, verbose_name='Категория')
-    variant_answer = models.ManyToManyField(VariantOfAnswer)
-    try_answer = models.ManyToManyField(VariantOfAnswer)
+        'Category', on_delete=models.RESTRICT, verbose_name='Категория')
+    variant_answers = models.ManyToManyField(VariantOfAnswer, through='QuestionVariantOfAnswer')
+
 
     def __str__(self):
         return self.body_question
@@ -36,9 +38,14 @@ class Question(BaseModel):
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
 
+class QuestionVariantOfAnswer(BaseModel):
+    # По сути сами создаём третью и явно указываем её в question.
+    question = models.ForeignKey('Question', on_delete=models.RESTRICT, verbose_name='Вопрос', related_name='')
+    variant_of_answer = models.ForeignKey('VariantOfAnswer', on_delete=models.RESTRICT, verbose_name='Вариант ответа')
+    is_correct = models.BooleanField(default=False)
 
 class Category(BaseModel):
-    title = models.TextField(blank=True, verbose_name='Категория')
+    title = models.TextField(verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -49,13 +56,22 @@ class Category(BaseModel):
 
 
 class AnswersFromHuman(BaseModel):
+    # question - id вопроса на который ответил пользователь
     question = models.ForeignKey(
-        'Question', on_delete=models.CASCADE, verbose_name='Вопрос')
-    # Вот тут чето не понятно.!!!
-    answer_from_user = models.ForeignKey(
-        'VariantOfAnswer', on_delete=models.CASCADE, verbose_name='Вариант ответа от пользователя')
-    # Вот тут чето не понятно.!!!
-
-
+        'Question', on_delete=models.RESTRICT, verbose_name='Вопрос на который отвечают')
+    # answers_from_user Many to Many - так как может быть много ответов на один вопрос.
+    answers_from_user = models.ManyToManyField(VariantOfAnswer, verbose_name='Вариант ответа пользователя')
+    complited_test = models.ForeignKey('ComlitedUserTest', on_delete=models.RESTRICT, verbose_name='Принадлежит тесту', related_name='answers')
+    
+class ComplitedUserTest(BaseModel):
+    test = models.ForeignKey('Test', on_delete=models.RESTRICT, verbose_name='Пройденный тест', related_name='complited_tests')
+    started_at = models.DateTimeField(auto_now_add=True, verbose_name='Время начало теста')
+    end_at = models.DateTimeField(verbose_name='Время окончания теста')
+    score_from_test = models.PositiveSmallIntegerField(verbose_name='Количество баллов за тест')
+    
 class Test(BaseModel):
-    pass
+    title = models.CharField(max_length=100, verbose_name='Название теста')
+    description = models.CharField(max_length=300, verbose_name='Описание теста')
+    time_for_test = models.DurationField()
+    # TODO один тест может содержать много одинаковых вопросов, подумать под unique?
+    question = models.ManyToManyField('Question', verbose_name='Вопрос', related_name='tests')
