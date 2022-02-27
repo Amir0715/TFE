a4310a5015a88fae378d63095d476d501da6f30e
amir kamolov <kamolov.amir2000@yandex.ru>
2022-02-27 22:18:49 +0300

    [+] Добавил пакеты

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { VuesticPlugin } from 'vuestic-ui' // <-
+import 'vuestic-ui/dist/vuestic-ui.css' // <-
+import Vuex from 'vuex'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VuesticPlugin, {})
+app.use(Vuex, {})
+app.mount('#app')
