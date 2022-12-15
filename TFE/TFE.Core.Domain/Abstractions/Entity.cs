7b05df391ae3db51df112fe0ce8d4a91a7fe785e
Kamolov Amir <kamolov.amir2000@yandex.ru>
2022-12-15 13:24:17 +0300

    [+] абстрактную сущнсость

---
+++ TFE/TFE.Core.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace TFE.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        protected Entity() { }
+        protected Entity(Guid id) => Id = id;
+
+        public Guid Id { get; protected set; }
+        public DateTime CreateDateTime { get; protected set; } = DateTime.Now;
+        public DateTime UpdateDateTime { get; protected set; } = DateTime.Now;
+    }
+}
---
+++ TFE/TFE.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TFE.Domain.Abstractions;
+
+namespace TFE.Persistence
+{
+    public class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) =>
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
